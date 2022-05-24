71adac78f7ece15d41f5d141e1f59b080339766b
ianjustinferris <ianjustinferris@gmail.com>
2022-05-24 17:49:18 -0400

    Delete function added to fsUtils.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const router = express.Router()
 //Destructuring objects from fsUtils
-const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, writeToFile, readAndAppend, readAndDelete} = require('../helpers/fsUtils');
 const { v4: uuidv4 } = require("uuid");
 
-// Notes route to respond as a GET request
+// Notes route to respond as a GET request and handled with readFromFile
 router.get('/notes', (req,res) => {
+
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
     
 });
 
-// Notes route to respond as a POST request
+// Notes route to respond as a POST request and handled with readAndAppend
 router.post('/notes', (req,res) => {
     const newNote = req.body;
 
@@ -20,6 +22,12 @@ router.post('/notes', (req,res) => {
     
 });
 
+// Notes route to respond as a DELETE request and handled with 
+router.delete('/notes/:id', (req,res) => {
+
+    readAndDelete(req.params.id, './db/db.json');
+    res.json({ ok: true });
+})
 
 module.exports = router;
 
